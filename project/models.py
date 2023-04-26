010775a22b659807df25ebc8fc72a744c12db22e
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-04-25 21:43:12 -0500

    first commit

---
+++ project/models.py
@@ -0,0 +1,59 @@
+
+from . import db
+from sqlalchemy.dialects.mysql import BLOB
+from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import Flask
+from sqlalchemy import DateTime
+
+class bids(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bid_number = db.Column(db.String(45))
+    title = db.Column(db.String(100))
+    type = db.Column(db.String(45))
+    organization = db.Column(db.String(100))
+    issue_date = db.Column(DateTime)
+    close_date = db.Column(DateTime)
+    notes = db.Column(db.String(16000000))
+    status = db.Column(db.String(45))
+
+class bid_contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bid_id = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    address_1 = db.Column(db.String(100))
+    address_2 = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(25))
+    phone = db.Column(db.String(20))
+    email = db.Column(db.String(45))
+
+class supplier_login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45))
+    password = db.Column(db.String(45))
+    email = db.Column(db.String(45))
+    supplier_id = db.Column(db.Integer)
+
+class supplier_info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45))
+    last_name = db.Column(db.String(45))
+    company_name = db.Column(db.String(256))
+    phone = db.Column(db.String(20))
+    duns = db.Column(db.String(12))
+    organization_type = db.Column(db.String(45))
+    tax_id = db.Column(db.String(12))
+    address_1 = db.Column(db.String(100))
+    address_2 = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(25))
+    commodity_code = db.Column(db.String(45))
+    business_classification = db.Column(db.String(45))
+
+class gov_login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45))
+    password = db.Column(db.String(45))
+    email = db.Column(db.String(45))
+
