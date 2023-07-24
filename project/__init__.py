fd86cc9e5766b5247e5c6608fd560abb079c709a
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-07-24 11:15:42 -0500

    Update to create custom error template. Also globally set user=current_user.

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from flask import Flask, session, request, redirect, url_for
+from flask import Flask, session, request, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from flask import g
@@ -92,6 +92,14 @@ def create_app():
                 user = None
             return user
 
+        # For each view function, assign user=current_user.
+        @app.context_processor
+        def inject_user():
+            if current_user.is_authenticated:
+                return dict(user=current_user)
+            else:
+                return dict(user=None)
+
         @app.before_first_request
         def set_user_type():
             session['user_type'] = None
@@ -101,6 +109,18 @@ def create_app():
             # Ensure that all requests are secure (HTTPS)
             if not request.is_secure and request.host != 'localhost:2000':
                 return redirect(request.url.replace('http://', 'https://'), code=301)
+
+        def handle_error(error):
+            error_code = getattr(error, 'code', 500)  # Get the error code, default to 500
+            return render_template('error.html', error_code=error_code), error_code
+
+        app.register_error_handler(404, handle_error)
+        app.register_error_handler(500, handle_error)
+        app.register_error_handler(400, handle_error)
+        app.register_error_handler(403, handle_error)
+        app.register_error_handler(401, handle_error)
+        app.register_error_handler(405, handle_error)
+        app.register_error_handler(503, handle_error)
 
         logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
