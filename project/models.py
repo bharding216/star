38b4b88b882ac449bd6333a68ad16bd50742ca2b
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2025-05-30 11:57:28 -0500

    Make ids auto increment

--- project/models.py
+++ project/models.py
@@ -4,7 +4,7 @@ from itsdangerous import URLSafeTimedSerializer as Serializer
 from sqlalchemy import DateTime, Date, Text
 
 class bids(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     bid_number = db.Column(db.String(45))
     title = db.Column(db.String(100))
     type = db.Column(db.String(45))
@@ -15,7 +15,7 @@ class bids(db.Model):
     status = db.Column(db.String(45))
 
 class bid_contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     bid_id = db.Column(db.Integer)
     name = db.Column(db.String(100))
     address_1 = db.Column(db.String(100))
@@ -26,19 +26,19 @@ class bid_contact(db.Model):
     email = db.Column(db.String(320))
 
 class supplier_login(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     password = db.Column(db.String(500))
     email = db.Column(db.String(320))
     supplier_id = db.Column(db.Integer, db.ForeignKey('supplier_info.id'))
     supplier = db.relationship('supplier_info', backref='supplier_login')
 
 class admin_login(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     password = db.Column(db.String(500))
     email = db.Column(db.String(320))
 
 class supplier_info(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(45))
     last_name = db.Column(db.String(45))
     company_name = db.Column(db.String(256))
@@ -55,7 +55,7 @@ class supplier_info(db.Model):
     zip_code = db.Column(db.String(25))
 
 class project_meta(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(500))
     uploaded_by_user_id = db.Column(db.Integer)
     date_time_stamp = db.Column(DateTime)
@@ -64,7 +64,7 @@ class project_meta(db.Model):
     bid = db.relationship('bids', backref='project_meta')
 
 class applicant_docs(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String(500))
     date_time_stamp = db.Column(DateTime)
     supplier_id = db.Column(db.Integer, db.ForeignKey('supplier_info.id'))
@@ -73,7 +73,7 @@ class applicant_docs(db.Model):
     bid = db.relationship('bids', backref='applicant_docs')
 
 class chat_history(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     author_type = db.Column(db.Integer)
     datetime_stamp = db.Column(DateTime)
     comment = db.Column(Text(length=2**24-1))
